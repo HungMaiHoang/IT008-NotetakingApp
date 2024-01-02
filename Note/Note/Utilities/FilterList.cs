183ad3728b0292de4e06a845e58773d3be0965ba
HungMaiHoang <22520510@gm.uit.edu.vn>
2024-01-02 20:09:41 +0700

    add filter

---
+++ Note/Note/Utilities/FilterList.cs
@@ -0,0 +1,30 @@
+﻿using Note.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.Utilities
+{
+    internal class FilterList
+    {
+        public static ObservableCollection<NoteModel> AscedingLastEdit(List<NoteModel> list)
+        {
+            return new ObservableCollection<NoteModel>(list.OrderBy(x => x.LastEdited).ToList());
+        }
+        public static ObservableCollection<NoteModel> DescendingLastEdit(List<NoteModel> list)
+        {
+            return new ObservableCollection<NoteModel>(list.OrderByDescending(x => x.LastEdited).ToList());
+        }
+        public static ObservableCollection<NoteModel> AscedingTitle(List<NoteModel> list)
+        {
+            return new ObservableCollection<NoteModel>(list.OrderBy(x => x.Title).ToList());
+        }
+        public static ObservableCollection<NoteModel> DescendingTitle(List<NoteModel> list)
+        {
+            return new ObservableCollection<NoteModel>(list.OrderByDescending(x => x.Title).ToList());
+        }
+    }
+}
